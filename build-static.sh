25d48cf07ca3acde392ecdf6dabb2035d6b15fe1
Russell Allen <mail@russell-allen.com>
2017-05-24 10:50:48 +1000

    Improved build-static.sh helper script

--- build-static.sh
+++ build-static.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
+
+#
+# USAGE:
+#
+# ./build-static.sh -tree /path/to/tree -snap myserver.snap
+#
+
+#
+# SETTINGS:
+#
+
 SELF=/usr/local/bin/Self
 BASE=../self/objects
 
-$SELF -f $BASE/worldBuilder.self -b $BASE -f2 setup.self -o none -f3 loadwebsite.self
-
-
+$SELF -f $BASE/worldBuilder.self -b $BASE -f2 setup.self -o none -f3 loadwebsite.self -tree $1 -snap $2
